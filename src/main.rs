891c6423b6a368603c57060da4882c89e397768d
MP <marc.picco@gmail.com>
2023-01-22 15:11:15 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,89 @@
+use std::fs;
+use dialoguer::Input;
+use rand::rngs::OsRng;
+use rand::rngs::adapter::ReseedingRng;
+use rand::prelude::*;
+use rand_chacha::ChaCha20Core; 
+
+fn main() {
+    // Request the file path from the user
+    let path = Input::<String>::new()
+        .with_prompt("Please enter file path")
+        .default(".".to_string())
+        .interact()
+        .unwrap();
+
+    println!("path {path}");
+
+    // Count the number of items in the directory
+    let path_clone = path.clone();
+    let item_count = fs::read_dir(path_clone)
+        .unwrap()
+        .count();
+
+    println!("item_count: {item_count}");
+
+    // Initialize a counter to track the number of files renamed
+    let mut counter = 0;
+
+    // Iterate over the items in the directory
+    for (index, entry) in fs::read_dir(path).unwrap().enumerate() {
+        println!("--");
+        let entry = entry.unwrap(); 
+
+        println!("index {index} __ counter {counter} ");
+
+        let path = entry.path();
+
+        // Check if the item is a file
+        if path.is_file() {
+            let file_name = path.file_name().unwrap().to_str().unwrap();
+            
+
+            // Define the valid file extensions
+            let file_exts = [
+                ".tif", 
+                ".exr", 
+                ".jpg", 
+                ".png", 
+                ".jpeg", 
+                ".gif", 
+                ".webp"
+            ];
+
+            // Check if the file has a valid extension
+            if file_exts.iter().any(|ext| file_name.ends_with(ext)) {
+                
+                // Generate a random number
+                let random_number = generate_random_number().to_string();
+
+                // Get the file extension
+                let file_ext = file_name.split(".").last().unwrap();
+
+                // Construct the new file name
+                let new_file_name = random_number + "." + file_ext;
+
+                println!("old --> new: {file_name} -- > {new_file_name} " );
+                if file_name.eq(&new_file_name){ 
+                    println!("....EQUAL...");
+                    
+                } else {
+                    // Construct the new file path
+                    let new_path = path.with_file_name(new_file_name);
+
+                    // Rename the file
+                    fs::rename(path, new_path).unwrap();
+                    counter  = counter +1;
+                }
+            }
+        }
+    }
+}
+
+// Function to generate a random number
+fn generate_random_number() -> u64 {
+    // Create a new random number generator
+    let prng = ChaCha20Core::from_entropy();
+    let mut reseeding_rng = ReseedingRng::new(prng, 0, OsRng); 
+    reseeding_rng.gen::<u64>()
+}
